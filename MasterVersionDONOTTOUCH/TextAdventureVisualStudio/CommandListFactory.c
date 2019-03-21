9bdac3b528a35178f7338eb8aa4e3042a566db4a
YuvalDigipen <48696077+YuvalDigipen@users.noreply.github.com>
2019-03-21 12:58:22 -0700

    Commands connected

--- MasterVersionDONOTTOUCH/TextAdventureVisualStudio/CommandListFactory.c
+++ MasterVersionDONOTTOUCH/TextAdventureVisualStudio/CommandListFactory.c
@@ -31,29 +31,38 @@ CommandList* CreateCommandList()
 				and add it to the list of commands for the game
 	   NOTICE:  It uses a function called HandleHelpCommand, you can find
 				additional command handler functions by looking here: CommandHandlerFunctions.h */
+
+	/*Yuval connect*/
 	CommandList_Add(cmdListPtr, "help", HandleHelpCommand, true);
 	/* TODO BASIC: add support for a short alias "h" */
 
-	/* TODO REQUIRED: create the "quit" command
-	TODO BASIC: add a short alias "q" */
+	/* TODO REQUIRED: create the "quit" command */
+	CommandList_Add(cmdListPtr, "quit", HandleQuitCommand, true);
+	/* TODO BASIC: add a short alias "q" */
 
-	/* TODO REQUIRED: create the "drop" command
-	TODO BASIC: add a short alias "d" */
+	/* TODO REQUIRED: create the "drop" command */
+	CommandList_Add(cmdListPtr, "drop", HandleDropCommand, true);
+	/* TODO BASIC: add a short alias "d" */
 
-	/* TODO REQUIRED: create the "take" command
-	TODO BASIC: add a short alias "t" */
+	/* TODO REQUIRED: create the "take" command */
+	CommandList_Add(cmdListPtr, "take", HandleTakeCommand, true);
+	/* TODO BASIC: add a short alias "t" */
 
-	/* TODO REQUIRED: create the "use" command
-	TODO BASIC: add a short alias "u" */
+	/* TODO REQUIRED: create the "use" command */
+	CommandList_Add(cmdListPtr, "use", HandleUseCommand, true);
+	/* TODO BASIC: add a short alias "u" */
 
-	/* TODO REQUIRED: create the "look" command
-	TODO BASIC: add a short alias "l" */
+	/* TODO REQUIRED: create the "look" command */
+	CommandList_Add(cmdListPtr, "look", HandleLookCommand, true);
+	/* TODO BASIC: add a short alias "l" */
 
-	/* TODO REQUIRED: create the "inventory" command
-	TODO BASIC: add a short alias "i" */
+	/* TODO REQUIRED: create the "inventory" command */
+	CommandList_Add(cmdListPtr, "inventory", HandleInventoryCommand, true);
+	/* TODO BASIC: add a short alias "i" */
 
-	/* TODO REQUIRED: create the "go" command
-	TODO BASIC: add a short alias "g" */
+	/* TODO REQUIRED: create the "go" command */
+	CommandList_Add(cmdListPtr, "go", HandleGoCommand, true);
+	/* TODO BASIC: add a short alias "g" */
 
 	/* TODO ADVANCED: create aditional commands
 	this should require the creation of additional .c files to implement the command functions */
